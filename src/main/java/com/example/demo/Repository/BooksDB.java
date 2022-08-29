5771dc1cb3a8c2bc7e4ed83c14916a0d36db03e5
anuragbhu <anurag.bhumca18@gmail.com>
2022-08-29 22:33:23 +0530

    Initial Commits

---
+++ src/main/java/com/example/demo/Repository/BooksDB.java
@@ -0,0 +1,55 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BooksDB {
+    ArrayList<Book> booksList = new ArrayList<>();
+
+    public String saveBook(Book book) {
+        for(Book books : booksList) {
+            if(books.getId() == book.getId())
+                return "Not able to store the book!";
+        }
+        booksList.add(book);
+        System.out.println(book + " is inserted.");
+        return "Able to store the book";
+    }
+
+    public List<Book> getBooks() {
+        return booksList;
+    }
+
+    public Book getBookById(long id) {
+        for(Book book : booksList) {
+            if(book.getId() == id)
+                return book;
+        }
+        return null;
+    }
+
+    public String updateBookDetails(Book book) {
+        for(Book books : booksList) {
+            if(books.getId() == book.getId()) {
+                booksList.remove(books);
+                booksList.add(book);
+                return "Book details updated successfully";
+            }
+        }
+        return "Book Id does not exist";
+    }
+
+    public String deleteBookDetails(long id) {
+        for(Book books : booksList) {
+            if(books.getId() == id) {
+                booksList.remove(books);
+                return "Book deleted successfully";
+            }
+        }
+        return "Book Id does not exist";
+    }
+}
---
+++ src/main/java/com/example/demo/Service/BookService.java
@@ -0,0 +1,35 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.Book;
+import com.example.demo.Repository.BooksDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    BooksDB booksDB;
+
+    public String insertBook(Book book) {
+        return booksDB.saveBook(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return booksDB.getBooks();
+    }
+
+    public Book getBookById(long id) {
+        return booksDB.getBookById(id);
+    }
+
+    public String updateBook(Book book) {
+        return booksDB.updateBookDetails(book);
+    }
+
+    public String deleteBook(long id) {
+        return booksDB.deleteBookDetails(id);
+    }
+}
